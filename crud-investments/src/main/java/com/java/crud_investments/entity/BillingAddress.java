94964851baa104ce4b81f19bed48d8de2d9b4e47
Pedro Afonso <83741439+pedroafon1@users.noreply.github.com>
2025-12-15 14:40:35 -0300

    add entities

---
+++ crud-investments/src/main/java/com/java/crud_investments/entity/BillingAddress.java
@@ -0,0 +1,58 @@
+package com.java.crud_investments.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_billingaddress")
+public class BillingAddress {
+
+    @Id
+    @Column(name = "account_id")
+    private UUID id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "number")
+    private String number;
+
+    public BillingAddress() {
+    }
+
+    public BillingAddress(UUID id, String street, String number) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
